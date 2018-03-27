d8f49929f246b5c9779b88704f5878cf2d7aca0a
arosales9 <arosales9@ucol.mx>
2018-03-26 19:19:54 -0600

    Primos

---
+++ Primos/Primos/Eratostenes.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primos
+{
+    class Eratostenes
+    {
+        public static int _max;
+
+        public Eratostenes(int num)
+        {
+            _max = num;
+        }
+
+        public string Primos()
+        {
+            int nume=0;
+            int[,] num = new int[_max, 2];
+            string primos = "";
+
+            for (int i = 2; i < _max; i++) //Rellenamos el array
+            {
+                num[i, 0] = i;
+                num[i, 1] = 0;
+            }
+
+            for (int i = 2; i < _max; i++)
+            {
+                if (num[i, 1] == 0)
+                {
+                    primos = primos + i.ToString() + ",";
+                    for (int e = 1; e < _max + 1; e++)
+                    {
+                        nume = num[i, 0];
+                        nume = nume * e;
+                        if (nume < _max)
+                        {
+                            num[nume, 1] = 1;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+
+            primos = primos.Substring(0, primos.Length - 1);
+            //Console.WriteLine("Numeros primos encontrados: {0}", primos);
+            //Console.Read();
+            return primos;
+        }
+    }
+}
---
+++ Primos/Primos/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Primos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            txtResultado.Clear();
+            Eratostenes Era = new Eratostenes(1000);
+            //MessageBox.Show("Primos : "+ Era.Primos());
+            txtResultado.Text = Era.Primos();
+        }
+    }
+}
